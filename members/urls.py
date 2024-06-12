134db89bc8a43b4bb68bfccc96669d98dcc181ed
syadav123 <sai.yadavalli1@gmail.com>
2024-06-12 12:01:08 -0500

    Created log out buttons for disciples and years views

--- members/urls.py
+++ members/urls.py
@@ -4,6 +4,6 @@ from . import views as members_views
 
 urlpatterns = [
     path('', members_views.homepage, name='homepage'),
-    path('disciples', members_views.disciples_page, name='disciples'),
-    path('years', members_views.years_page, name='years')
+    path('/disciples', members_views.disciples_page, name='disciples'),
+    path('/years', members_views.years_page, name='years')
 ]
